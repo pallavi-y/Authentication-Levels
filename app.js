fe5b9e916809450bf695e94f9894cbeeb615f26a
Pallavi <50053117+pallavi-y@users.noreply.github.com>
2020-08-12 16:02:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejs = require("ejs");
 const bcrypt = require("bcrypt");
 //The below required packages are used for cookies
 const session = require("express-session");
+//passport is an authentication middleware
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 
